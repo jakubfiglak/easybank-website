bbc6ab611f87f6cbc1fa9935aa18898de0739649
Jakub Figlak <jakub.figlak@gmail.com>
2020-04-18 13:15:30 +0200

    Set up project environment, add styled components and google fonts plugins

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  html {
+    font-size: 62.5%; 
+  }
+  
+  body {
+    font-size: 1.8rem;
+    font-family: ${({ theme }) => theme.fontFamily}
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/theme/Theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    primary: 'hsl(233, 26%, 24%)',
+    secondary: 'hsl(136, 65%, 51%)',
+    tertiary: 'hsl(192, 70%, 51%)',
+    grayishBlue: 'hsl(233, 8%, 62%)',
+    lightGrayishBlue: 'hsl(220, 16%, 96%)',
+    veryLightGray: 'hsl(0, 0%, 98%)',
+    white: 'hsl(0, 0%, 100%)',
+  },
+  fontSize: {
+    l: '1.8rem',
+  },
+  fontWeight: {
+    light: '300',
+    regular: '400',
+  },
+  fontFamily: '\'Public Sans\', sans-serif',
+};
